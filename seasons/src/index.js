ca7d3c00f3b3ac86c1cdd67176213f38d18f373e
Minh Huynh <minhhnn@gmail.com>
2019-09-16 23:10:10 +1000

    Displaying error message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,17 +4,23 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {lat: null};
+        this.state = {lat: null, errorMessage: ''};
         window.navigator.geolocation.getCurrentPosition(
             position => { 
                 this.setState({ lat: position.coords.latitude });
             },
-            error => console.log(error)
+            error => {
+                this.setState({errorMessage: error.message });
+            }
         );
     }
 
     render() {
-        return <div>Latitude: {this.state.lat}</div>;
+        return (<div>
+            Latitude: {this.state.lat}
+            <br/>
+            Error: {this.state.errorMessage}
+        </div>);
     }
 }
 
