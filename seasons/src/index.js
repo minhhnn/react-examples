48f8dcf0c1f91a2b2f3fcd3350d6f77928f436ef
Minh Huynh <minhhnn@gmail.com>
2019-09-16 22:31:01 +1000

    Implement Season Display using React Class Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {lat: null};
+        window.navigator.geolocation.getCurrentPosition(
+            position => { 
+                this.setState({ lat: position.coords.latitude });
+            },
+            error => console.log(error)
+        );
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
