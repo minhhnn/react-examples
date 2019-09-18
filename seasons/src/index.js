a1c0fb33d7022260c4db8e1e45239662372fe036
Minh Huynh <minhhnn@gmail.com>
2019-09-18 21:30:48 +1000

    Update Season Display to display correct season

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {lat: null, errorMessage: ''};
+    state = {lat: null, errorMessage: ''};
+    
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => { 
                 this.setState({ lat: position.coords.latitude });
@@ -16,11 +17,13 @@ class App extends React.Component {
     }
 
     render() {
-        return (<div>
-            Latitude: {this.state.lat}
-            <br/>
-            Error: {this.state.errorMessage}
-        </div>);
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage} </div>;
+        } 
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+        return <div>Loading...</div>;
     }
 }
 
