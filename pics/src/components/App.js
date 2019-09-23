11ac86e647a8cd497f7eec76bd4aff7bb667bc93
Minh Huynh <minhhnn@gmail.com>
2019-09-23 21:31:45 +1000

    Finish pics app

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-import './App.css'
+import './App.css';
+import ImageList from './ImageList';
 
-const App = () => {
-    return (
-        <div className="app ui container">
-            <SearchBar/>
-        </div>
-    );
-};
+class App extends React.Component {
+    state = {images: []};
+    //Use async to get response
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: {
+                query: term
+            }
+        });
+
+        this.setState({ images: response.data.results });
+    }
+
+    render() {
+        return (
+            <div className="app ui container">
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <ImageList images={this.state.images}/>
+            </div>
+        );
+    }
+}
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
     onInputChange(event) {
         console.log(event.target.value);
     }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    };
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange}/>
+                        <input type="text" value={this.state.term}
+                        onChange={(event) => this.setState({term: event.target.value})}/>
                     </div>
                 </form>
             </div>
