f997e76fbf57d88eac1429fb80fd3718e6204360
Minh Huynh <minhhnn@gmail.com>
2019-09-26 22:44:14 +1000

    Finalise song apps

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SongList from './SongList';
+import SongDetails from './SongDetails';
 
-const App = () => {
-    return <div>App</div>
-};
+class App extends Component {
+    render() {
+        return (
+            <div className="ui container grid">
+                <div className="ui row">
+                    <div className="column eight wide">
+                        <SongList />
+                    </div>
+                    <div className="column eight wide">
+                        <SongDetails />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
