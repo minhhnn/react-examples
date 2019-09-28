e8fdbfa0e5551e343c7544fc5c0c9ef489d6478c
Minh Huynh <minhhnn@gmail.com>
2019-09-28 22:05:41 +1000

    Create blog project

---
+++ songs/src/components/SongDetails.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetails = (props) => {
+    if (!props.song) {
+        return <div>Select a song</div>
+    }
+    const { title, duration } = props.song;
+    return (
+        <div>
+            <h3>Details for:</h3>
+            <p>
+                Title: { title }
+                <br />
+                Duration: { duration }
+            </p>
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+};
+
+export default connect(mapStateToProps)(SongDetails);
