733a5d961aa2b37dd04f153e6293bc7825762119
Minh Huynh <minhhnn@gmail.com>
2019-09-29 12:59:38 +1000

    Implement blog app with React-Redux Using redux thunk to hanld middleware

---
+++ blog/src/actions/index.js
@@ -0,0 +1,21 @@
+import jsonPlaceholder from '../api/jsonPlaceholder';
+
+export const fetchPosts = () => {
+    return async dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response.data
+        });
+    };
+};
+
+export const fetchUser = (userId) => {
+    return async dispatch => {
+        const response = await jsonPlaceholder.get(`/users/${userId}`);
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        });
+    };
+};
