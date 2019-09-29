75a7f1d0763fae6f2dea97ac604c711a5f4d5f2d
Minh Huynh <minhhnn@gmail.com>
2019-09-29 14:39:21 +1000

    Refactor code to use lodash chain

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,9 +2,14 @@ import _ from 'lodash';
 import jsonPlaceholder from '../api/jsonPlaceholder';
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    //Wait until fetchPosts finish before trying to get uniqe userId from those posts
     await dispatch(fetchPosts());
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(userId => dispatch(fetchUser(userId)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(userId => dispatch(fetchUser(userId)))
+        .value();
 }
 
 export const fetchPosts = () => {
