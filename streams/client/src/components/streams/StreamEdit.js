a25d7a9b9a0ce3563a865c5aed45bd16208b34d5
Minh Huynh <minhhnn@gmail.com>
2019-10-07 20:51:34 +1100

    Extract common code in StreamCreate to StreamForm so we can reuse in StreamEdit

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    );
+import { fetchStream } from '../../actions';
+
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    render() {
+        console.log(this.props.stream);
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div>{this.props.stream.title}</div>
+        );
+    }
 };
 
-export default StreamEdit;
+const mapStateToProp = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProp, { fetchStream })(StreamEdit);
